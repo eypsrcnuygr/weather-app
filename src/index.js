71db8b0e7e8ae7e44eb3344b33f67341b95bfe3b
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-11-19 17:55:27 +0300

    Solve env variables problem

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const envVariables = process.env.API_TOKEN;
 
-console.log(envVariables);
+const errorHandler = (event) => {
+  const errorDiv = document.querySelector('.error');
+  errorDiv.innerHTML = event.message;
+};
 
-// async function getReq() {
-//   try {
-//     const response = await fetch(
-//       `https://api.openweathermap.org/data/2.5/weather?q=istanbul&appid=${envVariables}`);
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (e) {
-//     console.log(e);
-//   }
-// }
+async function getReq(city) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${envVariables}&units=metric`,
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (e) {
+    alert(e)
+  }
+}
 
-// getReq();
+
+getReq('istanbul');
