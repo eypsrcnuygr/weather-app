d1edd284dc32d98d9844a984bc16d32006a949a7
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-11-20 12:55:31 +0300

    Add readme, add divs to content and add toggler button

---
+++ README.md
@@ -0,0 +1,53 @@
+# Project-Library
+
+This is the fourth project on the Microverse JavaScript curriculum, to learn asynchronous behaviours of Javascript and APIs.
+
+![screenshot](./src/assets/Screenshot1.png)<br>
+
+## Built With
+
+- JavaScript
+- ESLint
+- StyleLint
+- Openweathermap API
+
+## Prerequisities
+
+To get this project up and running locally, you must have node installed on your local machine.
+
+## Getting Started
+
+**Setup**
+
+- Clone this repository with ```git clone git@github.com:eypsrcnuygr/weather-app.git``` using your terminal or command line.<br>
+- Change to the project directory by entering ```cd weather-app``` in the terminal<br>
+- Next run ```npm install``` to install the necessary dependencies<br>
+- Run ```npm run watch``` to have the necessary changes applied if you want to change anything in the codebase and to have a main.js file. Without this command, the app won't run.
+- Finally run the 'index.html' with 'Live Server' to start the application.<br>
+
+## Authors
+
+👤 **Eyüp Sercan UYGUR**
+
+-   Github: [@eypsrcnuygr](https://github.com/eypsrcnuygr)
+-   Twitter: [@eypsrcnuygr](https://twitter.com/eypsrcnuygr)
+-   LinkedIn: [eypsrcnuygr](https://www.linkedin.com/in/eypsrcnuygr/)
+-   Email: [Eyüp Sercan UYGUR](sercanuygur@gmail.com)
+
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+-   This Project was part of an assignment available on The Odin Project.
+-   Our thanks to Microverse and all our peers and colleagues there.
+
+## 📝 License
+
+This project is [MIT](https://github.com/git/git-scm.com/blob/master/MIT-LICENSE.txt) licensed.
--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 const envVariables = process.env.API_TOKEN;
 
-const errorHandler = (event) => {
-  const errorDiv = document.querySelector('.error');
-  errorDiv.innerHTML = event.message;
-};
-
 async function getReq(city) {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${envVariables}&units=metric`,
     );
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (e) {
-    alert(e)
+    return e;
   }
 }
 
 
-getReq('istanbul');
+document.querySelector('.btn').addEventListener('click', () => {
+  const city = document.querySelector('.input').value;
+  (async () => {
+    const cityDetails = await getReq(city);
+    if (cityDetails.name) {
+      document.querySelector('.theData').innerHTML = `The weather is ${cityDetails.weather[0].main}`;
+      document.querySelector('.theData2').innerHTML = `${cityDetails.main.temp} average Temp Celcius`;
+      document.querySelector('.theData3').innerHTML = `${cityDetails.main.temp_max} max Temp Celcius`;
+      document.querySelector('.toggler').classList.remove('d-none');
+    } else {
+      document.querySelector('.error').innerHTML = cityDetails.message;
+    }
+  })();
+});
+
+document.querySelector('.toggler').addEventListener('click', (e) => {
+  e.preventDefault();
+  const reg1 = /Celcius/;
+  if (document.querySelector('.theData2').innerHTML.match(reg1)) {
+    document.querySelector('.theData2').innerHTML = `${((parseFloat(document.querySelector('.theData2').innerHTML) * (9 / 5) + 32)).toFixed(2)} average Temp Fah`;
+    document.querySelector('.theData3').innerHTML = `${((parseFloat(document.querySelector('.theData3').innerHTML) * (9 / 5) + 32)).toFixed(2)} max Temp Fah`;
+  } else {
+    document.querySelector('.theData2').innerHTML = `${(((parseFloat((document.querySelector('.theData2').innerHTML)) - 32) * (5 / 9))).toFixed(2)} average Temp Celcius`;
+    document.querySelector('.theData3').innerHTML = `${((((parseFloat(document.querySelector('.theData3').innerHTML)) - 32) * (5 / 9))).toFixed(2)} average Temp Celcius`;
+  }
+});
